2bb71e2d2c3687bfb4dbfa4ff48d3dff8b58b4cb
Bianca <87245838+B-Alm@users.noreply.github.com>
2022-07-25 16:04:49 -0300

    modulos-em-java-script

---
+++ Modulos-em-java-script/funcoes.mjs
@@ -0,0 +1,15 @@
+export function mostraIdade(nome, idade) {
+    return `A idade de ${nome} é ${idade}`;
+}
+
+export function mostraCidade(nome, cidade) {
+    return `A cidade de ${nome} é ${cidade}`;
+}
+
+export function mostraHobby(nome, hobby) {
+    return `A hobby de ${nome} é ${hobby}`;
+}
+
+//export { mostraIdade, mostraCidade, mostraHobby };
+
+
---
+++ Modulos-em-java-script/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Ste', 28));
+console.log(mostraCidade('Ste', 'Alagoas'));
+console.log(mostraHobby('Ste', 'ler livros'));
